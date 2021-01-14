70194f87f7605dee7501039458bbbfa1ce251ed0
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-14 16:11:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,19 +14,9 @@ end
 def assign_rooms(attendees)
   room = 0
   new = []
-  attendees.each do |person|
+  attendees.each do |name|
     room += 1
-    new << "Hello, #{person}! You have been assigned to room #{room}!"
+    new<<"Hello, #{name}! You'll be assigned to room #{room}!"
   end
   return new
 end
-
-# def assign_rooms(attendees)
-#   room = 0
-#   new = []
-#   attendees.each do |name|
-#     room += 1
-#     new<<"Hello, #{name}! You'll be assigned to room #{room}!"
-#   end
-#   return new
-# end
