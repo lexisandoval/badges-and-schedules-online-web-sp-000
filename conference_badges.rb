eb11df4917613fcee89d69aec49629604dfac710
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-14 15:52:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  new = []
+  attendees.each do |msgs|
+    new << badge_maker(msgs)
+  end
+  new
+end
+
+def assign_rooms(attendees)
+  batch_badge_creator(attendees)
+  
+end
